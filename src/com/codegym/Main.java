b368fb5766ab5e2742417108e7424598b2b6d384
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-06 14:27:15 +0700

    upload bai tap

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         String[] array = {"viet", "binh", "luyt", "Chien"};
+        String[] array2 = {"Minh", "Sang"};
         System.out.println("--Mang ban dau--");
         for (String element : array) {
             System.out.println(element);
@@ -16,6 +17,12 @@ public class Main {
         String[] newArray2 = removeElement(array, 2);
         System.out.println("--Mang sau khi xoa--");
         for (String element : newArray2) {
+            System.out.println(element);
+        }
+
+        String [] newArray3 = mergeArray(array,array2);
+        System.out.println("--Mang sau khi gop--");
+        for (String element : newArray3) {
             System.out.println(element);
         }
     }
@@ -41,6 +48,18 @@ public class Main {
                 newArray[i] = array[i];
             } else {
                 newArray[i] = array[i + 1];
+            }
+        }
+        return newArray;
+    }
+
+    public static String[] mergeArray(String[] array1, String[] array2){
+        String [] newArray = new String[array1.length + array2.length];
+        for (int i = 0; i < newArray.length ; i++) {
+            if (i < array1.length) {
+                newArray[i] = array1[i];
+            } else {
+                newArray[i] = array2[i - array1.length];
             }
         }
         return newArray;
